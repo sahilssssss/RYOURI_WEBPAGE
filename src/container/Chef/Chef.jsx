07dd73491753f9a6a9fdf63602cfb89209f7c086
Sahil Dey <139077358+sahilssssss@users.noreply.github.com>
2023-10-20 19:58:55 +0530

    Create Chef.jsx

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">Dear Esteemed Diners,</p>
+        </div>
+        <p className="p__opensans"> In the dual role of chef and restaurant founder, I warmly greet you. Our culinary voyage is a genuine expression of our devotion to exceptional tastes and dining moments. We're delighted to have you join us on this culinary expedition. </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
